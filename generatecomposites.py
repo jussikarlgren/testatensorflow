edd33f39a12230ac58b590d11ee154a20853da0d
Jussi <jussi@lingvi.st>
2018-12-29 15:40:39 +0100

    runs

--- generatecomposites.py
+++ generatecomposites.py
@@ -26,24 +26,25 @@ path = "/home/jussi/aktuellt/2018.recfut/tf_ner/data/recfut/new/"
 outpath = "/home/jussi/aktuellt/2018.recfut/tf_ner/data/recfut/"
 
 
-def mergefiles(wordfile, depfile, nerfile, posfile):
+def mergefiles(wordfile, posfile, depfile, nerfile):
     struct = []
     with open(wordfile,'r+') as f_words, open(posfile,'r+') as f_pos, open(depfile,"r+") as f_deps, open(nerfile,"r+") as f_ners:
-        for line_words, line_tags, line_deps, line_ners in zip(f_words, f_pos, f_deps, f_ners):
+        for line_words, line_pos, line_deps, line_ners in zip(f_words, f_pos, f_deps, f_ners):
             w1 = line_words.split()
-            p1 = line_tags.split()
+            p1 = line_pos.split()
             d1 = line_deps.split()
             n1 = line_ners.split()
-            struct.append(zip(w1, n1, d1, p1))
+            struct.append(zip(w1, p1, d1, n1))
     return struct
 
-categories = ["wordtokens", "ner", "dependencies", "postags"]
+categories = ["wordtokens", "postags", "dependencies", "ner"]
 MINCOUNT = 1
 joinstring = "#%#"
 
 def doallthefiles(rangelimit=4000):
     filelist = {}
     seenfile = {}
+    antal_frag = 0
     for ix in range(rangelimit):
         filelist[ix] = {}
         seenfile[ix] = True
@@ -55,17 +56,18 @@ def doallthefiles(rangelimit=4000):
             except:
                 seenfile[ix] = None
                 filelist[ix][cat] = None
+                del filelist[ix]
                 logger("index {} did not match up {} file: {}".format(ix, cat, fn), error)
-
-    conditions = ["wn", "wd", "wp", "wnd", "wnp", "wdp", "wndp"]
+    logger("antal filer: {}".format(len(filelist)), monitor)
+    conditions = ["wp", "wd", "wn", "wdp", "wnp", "wnd", "wndp"]
     vocabulary = {}
     vocabulary_words = Counter()
     vocabulary_labels = Counter()
-    vocabulary["wn"] = Counter()
-    vocabulary["wd"] = Counter()
     vocabulary["wp"] = Counter()
-    vocabulary["wnd"] = Counter()
+    vocabulary["wd"] = Counter()
+    vocabulary["wn"] = Counter()
     vocabulary["wnp"] = Counter()
+    vocabulary["wnd"] = Counter()
     vocabulary["wdp"] = Counter()
     vocabulary["wndp"] = Counter()
     outfrag = {}
@@ -73,14 +75,15 @@ def doallthefiles(rangelimit=4000):
         if seenfile[fileindex]:
             zippy = mergefiles(filelist[fileindex][categories[0]], filelist[fileindex][categories[1]],
                                filelist[fileindex][categories[2]], filelist[fileindex][categories[3]])
-            wn_f = open('{}{}/new_{:0>4d}.txt'.format(outpath, "wn", fileindex), "w+")
-            wd_f = open('{}{}/new_{:0>4d}.txt'.format(outpath, "wd", fileindex), "w+")
             wp_f = open('{}{}/new_{:0>4d}.txt'.format(outpath, "wp", fileindex), "w+")
-            wnd_f = open('{}{}/new_{:0>4d}.txt'.format(outpath, "wnd", fileindex), "w+")
+            wd_f = open('{}{}/new_{:0>4d}.txt'.format(outpath, "wd", fileindex), "w+")
+            wn_f = open('{}{}/new_{:0>4d}.txt'.format(outpath, "wn", fileindex), "w+")
             wnp_f = open('{}{}/new_{:0>4d}.txt'.format(outpath, "wnp", fileindex), "w+")
+            wnd_f = open('{}{}/new_{:0>4d}.txt'.format(outpath, "wnd", fileindex), "w+")
             wdp_f = open('{}{}/new_{:0>4d}.txt'.format(outpath, "wdp", fileindex), "w+")
             wndp_f = open('{}{}/new_{:0>4d}.txt'.format(outpath, "wndp", fileindex), "w+")
             for fragment in zippy:
+                antal_frag += 1
                 for cc in conditions:
                     outfrag[cc] = []
                 for oneitem in fragment:
@@ -88,25 +91,25 @@ def doallthefiles(rangelimit=4000):
                     vocabulary_labels.update([oneitem[1]])
                     vocabulary_labels.update([oneitem[2]])
                     vocabulary_labels.update([oneitem[3]])
-                    vocabulary["wn"].update([joinstring.join([oneitem[0], oneitem[1]])])
-                    outfrag["wn"].append("".join([oneitem[0], oneitem[1]]))
+                    vocabulary["wp"].update([joinstring.join([oneitem[0], oneitem[1]])])
+                    outfrag["wp"].append("".join([oneitem[0], oneitem[1]]))
                     vocabulary["wd"].update([joinstring.join([oneitem[0], oneitem[2]])])
                     outfrag["wd"].append("".join([oneitem[0], oneitem[2]]))
-                    vocabulary["wp"].update([joinstring.join([oneitem[0], oneitem[3]])])
-                    outfrag["wp"].append("".join([oneitem[0], oneitem[3]]))
-                    vocabulary["wnd"].update([joinstring.join([oneitem[0], oneitem[1], oneitem[2]])])
-                    outfrag["wnd"].append("".join([oneitem[0], oneitem[1], oneitem[2]]))
-                    vocabulary["wnp"].update([joinstring.join([oneitem[0], oneitem[1], oneitem[3]])])
-                    outfrag["wnp"].append("".join([oneitem[0], oneitem[1], oneitem[3]]))
+                    vocabulary["wn"].update([joinstring.join([oneitem[0], oneitem[3]])])
+                    outfrag["wn"].append("".join([oneitem[0], oneitem[3]]))
+                    vocabulary["wnp"].update([joinstring.join([oneitem[0], oneitem[1], oneitem[2]])])
+                    outfrag["wnp"].append("".join([oneitem[0], oneitem[1], oneitem[2]]))
+                    vocabulary["wnd"].update([joinstring.join([oneitem[0], oneitem[1], oneitem[3]])])
+                    outfrag["wnd"].append("".join([oneitem[0], oneitem[1], oneitem[3]]))
                     vocabulary["wdp"].update([joinstring.join([oneitem[0], oneitem[2], oneitem[3]])])
                     outfrag["wdp"].append("".join([oneitem[0], oneitem[2], oneitem[3]]))
                     vocabulary["wndp"].update([joinstring.join([oneitem[0], oneitem[1], oneitem[2], oneitem[3]])])
                     outfrag["wndp"].append("".join([oneitem[0], oneitem[1], oneitem[2], oneitem[3]]))
-                wn_f.write(" ".join(outfrag["wn"]) + "\n")
-                wd_f.write(" ".join(outfrag["wd"]) + "\n")
                 wp_f.write(" ".join(outfrag["wp"]) + "\n")
-                wnd_f.write(" ".join(outfrag["wnd"]) + "\n")
+                wd_f.write(" ".join(outfrag["wd"]) + "\n")
+                wn_f.write(" ".join(outfrag["wn"]) + "\n")
                 wnp_f.write(" ".join(outfrag["wnp"]) + "\n")
+                wnd_f.write(" ".join(outfrag["wnd"]) + "\n")
                 wdp_f.write(" ".join(outfrag["wdp"]) + "\n")
                 wndp_f.write(" ".join(outfrag["wndp"]) + "\n")
             wn_f.close()
@@ -117,7 +120,7 @@ def doallthefiles(rangelimit=4000):
             wdp_f.close()
             wndp_f.close()
 
-
+    logger("antal fragment: {}".format(antal_frag), monitor)
     vocab_words = {w for w, c in vocabulary_words.items() if c >= MINCOUNT}
     size_vocab = len(vocab_words)
     logger("antal ord std: {}".format(size_vocab), monitor)
@@ -152,7 +155,7 @@ def doallthefiles(rangelimit=4000):
                 f.write("{} {}\n".format(www, " ".join(map(str, compositeembeddings[www]))))
 
 
-howmany = 19
+howmany = 2937
 # read words file
 if __name__ == '__main__':
 
